7ba3a11a344c623310d19a8221d223e76e8be458
Leo <lechewu@gmail.com>
2023-04-12 00:20:46 +0800

    feat: apply redux

--- src/components/TrendingTags/TrendingTags.tsx
+++ src/components/TrendingTags/TrendingTags.tsx
@@ -1,20 +1,47 @@
 /** @jsxImportSource @emotion/react */
-import React, { useMemo } from 'react'
+import React, { useContext, useMemo, useEffect, useCallback } from 'react'
 import { useTags } from 'src/hooks'
+import { AppContext } from 'src/components'
 import { trendingTagsContainer } from './styles'
 
 const TrendingTags = () => {
   const { tags } = useTags()
+  const {
+    state: { selectedTag, searchTag },
+    actions: { setSelectedTag },
+  } = useContext(AppContext)
+
+  const onClickTag = useCallback(
+    (tag: string) => {
+      setSelectedTag(tag)
+    },
+    [setSelectedTag],
+  )
 
   const Tags = useMemo(
     () =>
-      tags.map((tag) => (
-        <div key={tag} className="tagContainer">
-          {tag}
-        </div>
-      )),
-    [tags],
+      tags
+        .filter((tag) => {
+          return tag.includes(searchTag)
+        })
+
+        .map((tag) => (
+          <div
+            key={tag}
+            className={`tagContainer ${selectedTag === tag ? 'selected' : ''}`}
+            onClick={onClickTag.bind(null, tag)}
+          >
+            {tag}
+          </div>
+        )),
+    [onClickTag, searchTag, selectedTag, tags],
   )
+
+  useEffect(() => {
+    if (!selectedTag && tags.length) {
+      setSelectedTag(tags[0])
+    }
+  }, [selectedTag, setSelectedTag, tags, tags.length])
 
   return (
     <div css={trendingTagsContainer}>
