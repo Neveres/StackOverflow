9e106afdece9c0f612ccabe2016d36377a74f3da
Leo <lechewu@gmail.com>
2023-04-12 14:53:34 +0800

    test: add tests

---
+++ src/hooks/__test__/useTags.test.ts
@@ -0,0 +1,30 @@
+import { waitFor } from '@testing-library/react'
+import { renderHook } from '@testing-library/react-hooks'
+import { useTags } from 'src/hooks'
+
+const response = {
+  data: {
+    items: [
+      {
+        id: 'ace46791-391c-4bc4-b6cf-e2150491ac18',
+        name: 'Rustic Fresh Cheese',
+        count: 0,
+      },
+    ],
+  },
+}
+
+jest.mock('src/libraries', () => ({
+  ...jest.requireActual('src/libraries'),
+  httpClient: { get: () => new Promise((resolve) => resolve(response)) },
+}))
+
+describe('useTags', () => {
+  test('should return expected tags', async () => {
+    const { result } = renderHook(useTags)
+
+    await waitFor(() => {
+      expect(result.current.tags).toStrictEqual(['Rustic Fresh Cheese'])
+    })
+  })
+})
